0b895ea1c6db4150f1f186f7b922c6f1ea96e838
Pavel Konovalov <ljadrbln@gmail.com>
2025-08-18 17:40:52 +0300

    refactor(tests): Move AddEntryTest under Entries namespace

--- backend/tests/Unit/Application/UseCases/Entries/AddEntryTest.php
+++ backend/tests/Unit/Application/UseCases/Entries/AddEntryTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Daylog\Tests\Unit\Application\UseCases;
+namespace Daylog\Tests\Unit\Application\UseCases\Entries;
 
 use Codeception\Test\Unit;
 use Daylog\Application\DTO\Entries\AddEntryRequest;
@@ -11,7 +11,6 @@ use Daylog\Application\Validators\Entries\AddEntryValidatorInterface;
 use Daylog\Application\Exceptions\DomainValidationException;
 use Daylog\Application\UseCases\Entries\AddEntry;
 use Daylog\Domain\Interfaces\Entries\EntryRepositoryInterface;
-use Daylog\Domain\Models\Entries\Entry;
 use Daylog\Tests\Support\Helper\EntryHelper;
 use Daylog\Domain\Services\UuidGenerator;
 
@@ -29,45 +28,6 @@ use Daylog\Domain\Services\UuidGenerator;
  */
 final class AddEntryTest extends Unit
 {
-    /**
-     * Happy path: validator passes, repository called, UUID returned.
-     */
-    // public function testHappyPathSavesEntryAndReturnsUuid(): void
-    // {
-    //     /** Arrange **/
-    //     $data  = EntryHelper::getData();
-
-    //     /** @var AddEntryRequestInterface $request */
-    //     $request = AddEntryRequest::fromArray($data);
-
-    //     $repoClass = EntryRepositoryInterface::class;
-    //     $repoMock  = $this->createMock($repoClass);
-
-    //     $expectedUuid = 'mocked-uuid';
-    //     $repoMock
-    //         ->expects($this->once())
-    //         ->method('save')
-    //         ->with($this->isInstanceOf(Entry::class))
-    //         ->willReturn($expectedUuid);
-
-    //     $validatorInterface = AddEntryValidatorInterface::class;
-    //     $validatorMock      = $this->createMock($validatorInterface);
-
-    //     $validatorMock
-    //         ->expects($this->once())
-    //         ->method('validate')
-    //         ->with($request);
-
-    //     $uc = new AddEntry($repoMock, $validatorMock);
-
-    //     /** Act **/
-    //     $uuid = $uc->execute($request);
-
-    //     /** Assert **/
-    //     $this->assertSame($expectedUuid, $uuid);
-    // }
-
-
     /**
      * Happy path: validates request, delegates save to repository, and returns a response DTO.
      *
