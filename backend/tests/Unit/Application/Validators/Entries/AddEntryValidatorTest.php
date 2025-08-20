ae8c2da698347fc1711e07c0a3a65adade31b250
Pavel Konovalov <ljadrbln@gmail.com>
2025-08-20 17:15:59 +0300

    refactor(tests): move AddEntryValidatorTest under Entries namespace

--- backend/tests/Unit/Application/Validators/Entries/AddEntryValidatorTest.php
+++ backend/tests/Unit/Application/Validators/Entries/AddEntryValidatorTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Daylog\Tests\Unit\Application\Validators;
+namespace Daylog\Tests\Unit\Application\Validators\Entries;
 
 use Codeception\Test\Unit;
 use Daylog\Domain\Models\Entries\EntryConstraints;
