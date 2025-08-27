108651a06e89b590e80baf335665a818adc0e793
Pavel Konovalov <ljadrbln@gmail.com>
2025-08-27 14:04:44 +0300

    refactor(tests): change namespace directory name

--- backend/tests/Integration/Infrastructure/Storage/Entries/EntryModelTest.php
+++ backend/tests/Integration/Infrastructure/Storage/Entries/EntryModelTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Daylog\Tests\Integration\Infrastructure\Storage\Entry;
+namespace Daylog\Tests\Integration\Infrastructure\Storage\Entries;
 
 use Codeception\Test\Unit;
 use Daylog\Infrastructure\Storage\Entries\EntryModel;
